ea4dc3242d3af38f8d3af25ee069fb26465f2a50
garthdb <garthdb@gmail.com>
2016-10-28 04:20:12 -0600

    :books: docs(Atomdoc): added atomdoc style documentation to the functions.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,82 @@
+/**
+ *  Public: format the long name of an option.
+ *
+ *  * `val` {String} the option's long name.
+ *
+ *  ## Examples
+ *
+ *  ```js
+ *  formatLong('--foo-bar'); // returns 'foo-bar'
+ *  ```
+ *
+ *  Returns formatted {String} of the long name.
+ */
 export function formatLong(val) {
   return val
     .replace('--', '')
     .replace('no-', '');
 }
 
+/**
+ *  Public: formats the short name of an option.
+ *
+ *  * `val` {String} the option's short name.
+ *
+ *  ## Examples
+ *
+ *  ```js
+ *  formatShort('-f'); // returns 'f'
+ *  ```
+ *
+ *  Returns formatted {String} of the short name.
+ */
 export function formatShort(val) {
   return val.replace('-', '');
 }
 
-export function camelcase(flag) {
-  return flag.split('-').reduce((str, word) =>
+/**
+ *  Public: converts hyphenated strings to camelcase.
+ *
+ *  * `val` hyphenated {String}
+ *
+ *  ## Examples
+ *
+ *  ```js
+ *  camelcase('foo-bar'); // returns 'fooBar'
+ *  ```
+ *
+ *  Returns camelcased {String}
+ */
+export function camelcase(val) {
+  return val.split('-').reduce((str, word) =>
     str + word[0].toUpperCase() + word.slice(1)
   );
 }
 
+/**
+ *  Public: a parser to make it use commander.js parsed arguments in rc.
+ *
+ *  * `program` commander.js {Command} object.
+ *
+ *  ## Examples
+ *
+ *  ```js
+ *  var program = require('commander');
+ *  var argParser = require('commander-rc-adapter');
+ *  var rc = require('rc');
+ *
+ *  program
+ *    .version('0.0.1')
+ *    .option('-f, --foo-doo', 'add some foo')
+ *    .option('-b, --bar <string>', 'add some bar');
+ *  program.parse(args);
+ *
+ *  var defaults = {};
+ *  var options = rc('appname', defaults, argParser(program));
+ *  ```
+ *
+ *  Returns {Object} of arguments formatted for rc.
+ */
 export default function adapter(program) {
   const result = { _: program.args };
   program.options.forEach((option) => {
